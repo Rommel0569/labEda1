d571788d7082e60f9d25a8e4d2b7c6cd3a45f105
Rommel0569 <rchambive@unsa.edu.pe>
2024-05-04 15:22:24 -0500

    Bienvenido al cursogit status!

--- Eda.java
+++ Eda.java
@@ -3,5 +3,7 @@ import java.util.*;
 public class Eda {
     public static void main(String []args){
         System.out.println("Hola mundo");
+        System.out.println("Bienvenido al curso");
+        
     }
 }
