7df586dba531e5c145c9a1212b90d5be58386200
Rommel0569 <rchambive@unsa.edu.pe>
2024-05-04 15:29:42 -0500

    Rommel Abraham Chambi Velasquez

--- Eda.java
+++ Eda.java
@@ -4,6 +4,6 @@ public class Eda {
     public static void main(String []args){
         System.out.println("Hola mundo");
         System.out.println("Bienvenido al curso");
-        
+        System.out.println("Rommel Abraham Chambi Velasquez");
     }
 }
